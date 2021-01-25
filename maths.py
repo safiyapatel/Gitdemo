cf630d19c76a33e65a67189ae85341c54166735b
safiya patel <patelsafiya166@gmail.com>
2021-01-25 14:59:04 +0530

    Added math.py to RE

---
+++ maths.py
@@ -0,0 +1,19 @@
+# Perform addition
+def add(x,y):
+    pass
+# Perform subtraction
+def subtract(x,y):
+    pass
+# Perform multiplication
+def multiply(x,y):
+    return x*y
+    
+# Perform division
+def divide(x,y):
+    return x/y
+
+<<<<<<< HEAD
+=======
+def square(x):
+	return x*x
+>>>>>>> BugFix03
